743c7c517f71bf24f91b8372388d3a8dcb77bc9a
Navaneeth Gade <navaneethgade07@gmail.com>
2026-03-30 00:02:05 +0530

    Updatin prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,10 @@
+// prisma.config.ts
+
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "src/db/schema.prisma",
+  schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
